c8611192b2d8c4dffcd1cd6282833d6368f6aacc
Shrabanya <workitupbanya8@gmail.com>
2026-08-24 21:01:07 +0530

    added some changes

--- expense.py
+++ expense.py
@@ -1,7 +1,25 @@
-class expenseManager:
-    def __init__(self):
-        pass
-    def add_expanse(self):
-        pass
+class ExpenseManager:
+
+    def __init__(self,items,amount, category, description, data_time):
+        self.expenses=items
+        self.amounts=amount
+        self.category=category
+        self.des=description
+        self.details=data_time
+
+    def cal_expanse(self, amount):
+        
+        self.amounts+=amount
+
     def show_expense(self):
-        pass
+        print("Item:", self.expenses)
+        print("Amount:", self.amounts)
+        print("Category:", self.category)
+        print("Description:", self.des)
+        print("Date & Time:",self.details)
+
+
+objexpense=ExpenseManager(1,250, "FOOD", "Dinner","17th Aug- 10:12PM")
+print(objexpense.show_expense())
+objexpense.cal_expanse(23)
+print(objexpense.show_expense())
